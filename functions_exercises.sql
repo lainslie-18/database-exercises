48a7189803229cd891d007c91de1fdb3a2104fd5
Lori Ainslie <84601320+lainslie-18@users.noreply.github.com>
2021-09-21 16:12:39 -0400

    Update functions_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -128,7 +128,8 @@ where birth_date like '%12-25' and hire_date like '199%';
 
 -- 5. Find the smallest and largest current salary from the salaries table.
 select min(salary) as min_salary, max(salary) as max_salary
-from salaries;
+from salaries
+where to_date > curdate();
 
 -- 6. Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born.
 select lower(concat(substr(first_name,1,1),substr(last_name,1,4),'_',substr(birth_date,6,2),substr(birth_date,3,2)))
